29f84d12d77b359c298f69f84eb6165db8bea278
aaqibb13 <aaqibb13@gmail.com>
2021-04-17 23:36:30 +0530

    Updated all

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Aaqib_Bashir',
+        'USER': 'aaqibb13',
+        'PASSWORD': 'Onanpariahsarebeautiful',
+        'HOST':'localhost',
+        'POSRT': '5432',
     }
 }
 
