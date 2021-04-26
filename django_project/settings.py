8d65a318a50ed18b40a626a527ef1e9be312acc9
aaqibb13 <aaqibb13@gmail.com>
2021-04-26 22:44:22 +0530

    DEBUG settings changed

--- django_project/settings.py
+++ django_project/settings.py
@@ -22,8 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'i_+4p-ndz9uy@^(h^n@da1r-$6$&!3p#zjjhjt)-2#9xc661xx'
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
